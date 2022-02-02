0e22661a505013f74e15bcaf7382d6472b4d613f
Alexei Latushko <alexei.latushko@banuba.com>
2022-02-02 17:14:28 +0300

    Move sufrace_destroyed() to render thread

--- offscreen_effect_player/offscreen_effect_player.cpp
+++ offscreen_effect_player/offscreen_effect_player.cpp
@@ -44,9 +44,10 @@ namespace bnb::oep
     /* offscreen_effect_player::~offscreen_effect_player */
     offscreen_effect_player::~offscreen_effect_player()
     {
-        m_ep->surface_destroyed();
-        // Deinitialize offscreen render target, should be performed on render thread.
+        // Switches playback state to inactive state and deinitializes offscreen render target.
+        // Must be performed on render thread.
         auto task = [this]() {
+            m_ep->surface_destroyed();
             m_ort->deinit();
         };
         m_scheduler.enqueue(task).get();
